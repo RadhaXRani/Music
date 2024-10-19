881d5d306db4df53444ed9682f84c13040b708d3
RadhaXRani <radha11189198141@gmail.com>
2024-10-19 16:58:43 +0530

    Update bot.py

--- ERAVIBES/core/bot.py
+++ ERAVIBES/core/bot.py
@@ -33,7 +33,7 @@ class ERABot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "ERAVIBES",
+            "HAPPYVIBES",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
